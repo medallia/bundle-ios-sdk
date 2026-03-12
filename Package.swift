4e815b201dbed096c6a3a73c3d1c835f2824eeb4
Jenkins <jenkins@jenkins.local>
2026-03-12 12:22:32 -0600

    Jenkins Release Job 19 message: chore(release): 1.0.0-alpha.7

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "medallia-bundle-ios-sdk",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(name: "MedalliaDigital", targets: ["MedalliaDigitalWrapper"]),
+        .library(name: "MedalliaDXA", targets: ["MedalliaDXAWrapper"])
+    ],
+    dependencies: [
+        .package(name: "digital-ios-sdk", url: "https://github.com/medallia/digital-ios-sdk", .upToNextMajor(from: "4.10.0")),
+        .package(name: "dxa-ios-sdk", url: "https://github.com/medallia/dxa-ios-sdk", .upToNextMajor(from: "3.8.0"))
+    ],
+    targets: [
+        .target(name: "MedalliaDigitalWrapper", dependencies: [
+            .product(name: "MedalliaDigitalSDK", package: "digital-ios-sdk")
+        ], path: "Sources/MedalliaDigitalWrapper"),
+        .target(name: "MedalliaDXAWrapper", dependencies: [
+            .product(name: "medallia-dxa-ios-sdk", package: "dxa-ios-sdk"),
+            .product(name: "medallia-dxa-ios-objc-sdk", package: "dxa-ios-sdk")
+        ], path: "Sources/MedalliaDXAWrapper")
+    ]
+)
